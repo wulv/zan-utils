41a53e7501f171f188e800fc7cc1ec90f65821bb
kainstar <461345042@qq.com>
2018-10-19 21:50:08 +0800

    add gulp typing ts export plugin

--- tasks/typing.js
+++ tasks/typing.js
@@ -1,9 +1,10 @@
 import gulp from 'gulp';
 import { createProject } from 'gulp-typescript';
 import config from '../config';
+import dtsExport from './plugins/dts-export';
 
 gulp.task('typing', () => {
   const tsProject = createProject('tsconfig.json', config.dtsTsconfig);
   const tsResult = tsProject.src().pipe(tsProject());
-  return tsResult.dts.pipe(gulp.dest(config.dist));
+  return tsResult.dts.pipe(dtsExport()).pipe(gulp.dest(config.dist));
 });
