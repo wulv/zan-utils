4aba22ee5006fe02fe7329369362e4e9e6f65d6d
yangyazhou <yangyazhou@youzan.com>
2018-10-26 16:11:25 +0800

    [fix] ci upgrade

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   coverageReporters: ["html", "json"],
